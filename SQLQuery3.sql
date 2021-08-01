e55ae47befb9eb46749142ff6555fc8296ddec68
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-08-01 21:00:42 +0530

    [Vedhashhni] Updated the file with table

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -3,3 +3,18 @@
 create database AddressBookSystem;
 
 use AddressBookSystem;
+
+-----------------UC2----------------
+
+create table Address_Book_Table
+(
+FirstName varchar(50) not null,
+LastName varchar(20),
+Address varchar(100),
+City varchar(50),
+StateName varchar(55),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
+
